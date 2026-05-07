7b8d0d62271bf393587af6be6d1373ef3c0d1862
mulaihongliqi627-lgtm <mulaihongliqi627@gmail.com>
2026-05-07 19:37:59 +0800

    动态规划, 卡特兰数

---
+++ dp/src/Knapsack/demo8.java
@@ -0,0 +1,16 @@
+package Knapsack;
+
+//动态规划: 卡特兰数
+//https://leetcode.cn/problems/unique-binary-search-trees/
+public class demo8 {
+    public int numTrees(int n) {
+        int []dp = new int [n+1];
+        dp[0] = 1;
+        for(int i = 1;i <= n;i++){
+            for(int j = 1;j <= i;j++){
+                dp[i] += dp[j-1]*dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+}
